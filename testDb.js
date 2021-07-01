7d7f1ad6dee7eb851ddab5301e38f71c81eaea86
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-01 13:41:43 -0500

    Inserted first collection.

--- testDb.js
+++ testDb.js
@@ -1,2 +1,25 @@
 const db = require('./models')
 db.connect()
+
+const drinkCRUD = async () => {
+    try {
+        // CREATE
+        const newDrink = await new db.Drink({
+            name: "Chocolate Milk",
+            rating: 9
+        })
+
+        await newDrink.save()
+
+        console.log(`new drink: ${newDrink}`)
+        // READ
+
+        // UPDATE
+
+        // DESTROY
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+drinkCRUD()
